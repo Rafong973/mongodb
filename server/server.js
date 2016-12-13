cfaa83d4d56f36c7f7403f8529af0b2bb3b1ce1b
rafong973 <zrfmyself@gmail.com>
2016-12-13 18:06:33 +0800

    登陆完成

--- server/server.js
+++ server/server.js
@@ -1,9 +1,25 @@
 'use strict';
 
+var user = require('./mongo');
+
 module.exports = function server(app,body){
 	app.post('/login',body.urlencoded(),function(req,res){
-		console.log(body)
-		console.log(req.body);
-		res.send('welcome')
+		const data = req.body;
+		const db = user.Kitten;
+		var msg = "";
+		if (!data) return res.sendStatus(400);
+		db.find({},function(err,docs){
+			for(var i=0;i<docs.length;i++){
+				console.log(docs[i]);
+				if(docs[i].name == data.u){
+					msg = {status:0,msg:"no this user"};
+					break;
+				}else{
+					msg = {status:1,msg:"u r welcome"};
+				}
+			}
+			res.send(msg);
+		});
+		console.log(msg);
 	})
 }
